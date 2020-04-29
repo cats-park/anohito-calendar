ce23470ea8b9eb6ef1d6ba3cb8c1abb382e08a09
cats-park <sakaiicats@gmail.com>
2020-04-29 22:29:39 +0900

    フォーマット作成画面作成途中

--- management/formatMake.js
+++ management/formatMake.js
@@ -1,30 +1,50 @@
-let req = new XMLHttpRequest();						// XMLHttpRequest オブジェクトを生成する
 
-// 対象のURLをinputから受け取り変数に代入する
+
+const result = document.getElementsByClassName("result");
 
 const makeBtn = document.getElementById("make_btn");
 
-makeBtn.addEventListener("click", () => {
-    // const url = "http://anohito-calendar.jp/index.html#request";
-    //
-    // req.open("GET", url);
-    // req.send();
-    //
-    // req.onreadystatechange = function() {				// XMLHttpRequest オブジェクトの状態が変化した際に呼び出されるイベントハンドラ
-    // 	if(req.readyState == 4 && req.status == 200){	// サーバーからのレスポンスが完了し、かつ、通信が正常に終了した場合
-    //         let json = req.responseText;
-    //         console.log(json);
-    //     }
-    // };
-    $.ajax({
-        url: 'https://twitcasting.tv/uni_mafumafu/movie/608580223',
-        type: 'GET',
-        dataType: 'html',
-    })
-    .done(function(data) {
-        $("#output").html($(data).find('#movietitle').text());
-    })
-    .fail(function( data ) {
-        // ...
-    });
+
+makeBtn.addEventListener("click", function() {
+    let title = document.getElementById("title");
+    let url = document.getElementById("url");
+
+    if(title.value !="" && url.value !="") {
+        title.style.border = "none";
+        url.style.border = "none";
+
+        makeFormat(title.value, url.value);
+    }else {
+        // タイトルが未入力の場合赤枠で囲む
+        if(title.value =="") {
+            title.style.border = "solid 3px red";
+        }
+        // URLが未入力の場合赤枠で囲む
+        if(url.value =="") {
+            url.style.border = "solid 3px red";
+        }
+
+    }
 });
+
+
+
+let makeFormat = (title, url) => {
+    let medium = "";
+    // youtube
+    if(url.indexOf("youtube") != -1) {medium = "YouTube"}
+    // ツイキャス
+    if(url.indexOf("twitcasting") != -1) {medium = "ツイキャス"}
+    // OPENREC
+    if(url.indexOf("openrec") != -1) {medium = "OPENREC"}
+    // niconico
+    if(url.indexOf("nicovideo") != -1) {medium = "niconico"}
+    // mildom
+    if(url.indexOf("mildom") != -1) {medium = "mildom"}
+
+    result.insertAdjacentHTML("afterbegin",
+    `
+    <p>${title}</p>
+    `
+    );
+}
